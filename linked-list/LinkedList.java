6d8a0ddc677db1447b00befb4e35191370127158
Michael Verdicchio <verdicchio@gmail.com>
2019-01-16 11:31:24 -0500

    Update LinkedList.java

--- linked-list/LinkedList.java
+++ linked-list/LinkedList.java
@@ -1,96 +1,83 @@
+
+
 import java.util.Iterator;
 
-public class LinkedList<AnyType> implements Iterable<AnyType> {
+public class LinkedList<Item> implements Iterable<Item> {
 
-	// here are the instance variables/data fields for the list
-	private int size;
-	private Node<AnyType> first;
+  private int size;
+  private Node<Item> first;
 
-	// here are the instance methods for the class
+  // no constructor because default is fine.
 
-	// no constructor needed since default constructor is fine.
+  public boolean isEmpty() {
+    return first == null;
+  }
 
-	public int size() {
-		return size;
-	}
+  public int size() {
+    return size;
+  }
 
-	public boolean isEmpty() {
-		return first == null;
-	}
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Iterator<Item> iter = this.iterator();
+    while (iter.hasNext())
+      sb.append(iter.next().toString() + "\n");
+    return sb.toString();
+  }
 
-	public void add(AnyType item) {
-		Node<AnyType> oldFirst = first;
-		first = new Node<AnyType>();
-		first.item = item;
-		first.next = oldFirst;
-		size++;
-	}
+  // add to beginning
+  public void add(Item item) {
+    Node<Item> oldFirst = first;
+    first = new Node<Item>();
+    first.item = item;
+    first.next = oldFirst;
+    size++;
+  }
 
-	// only removes the first one.
-	public void remove() {
-		first = first.next;
-		size--;
-	}
+  // remove from beginning
+  public void remove() {
+    first = first.next;
+    size--;
+  }
 
-	public String toString() {
-		// perhaps re-define this to use the iterator instead of doing it
-		// manually.
-		StringBuilder sb = new StringBuilder();
-		for (Node<AnyType> x = first; x != null; x = x.next) {
-			sb.append(x.item + "\n");
-		}
-		return sb.toString();
-	}
 
-	// here's the method we must write to call ourselves Iterable
-	// it's only job is to return an object that knows how to iterate over this
-	// list.
-	@Override
-	public Iterator<AnyType> iterator() {
-		return new SuperFancyIterator<AnyType>(first);
-	}
+  // --- homework LL methods go in here
 
-	// here come the inner classes used by the list.
 
-	/*
-	 * we don't need to write a constructor -- the default one is fine. we don't
-	 * need getters/setters since it's a nested class. private means no other
-	 * class can use this Node type static means this inner class can only
-	 * access static members of the list class
-	 */
-	private static class Node<AnyType> {
-		private AnyType item;
-		private Node<AnyType> next;
-	}
+  // ---
 
-	/*
-	 * this class defines an iterator that knows how to traverse this
-	 * collection. an iterator is like an arrow you advance through a collection
-	 * one by one. in our case, our iterator knows that our data items are
-	 * trapped inside of our linked list nodes.
-	 */
-	private static class SuperFancyIterator<AnyType> implements Iterator<AnyType> {
-		// the only instance variable is the reference to the next node to
-		// process
-		private Node<AnyType> nextNode;
+  @Override
+  public Iterator<Item> iterator() {
+    return new FancyIterator<Item>(first);
+  }
 
-		// the constructor needs a reference to the first node in the list to
-		// set things up.
-		public SuperFancyIterator(Node<AnyType> startNode) {
-			nextNode = startNode;
-		}
+  // helper static classes to follow.
 
-		public boolean hasNext() {
-			return nextNode != null;
-		}
+  // this is a node.
+  private static class Node<Item> {
+    Item item;
+    Node<Item> next;
+  }
 
-		// this method is careful to return the item, not the node itself.
-		public AnyType next() {
-			Node<AnyType> save = nextNode;
-			nextNode = nextNode.next;
-			return save.item;
-		}
+  // this is an iterator class for Items in this linked list.
+  private static class FancyIterator<Item> implements Iterator<Item> {
+    private Node<Item> nextNode;
 
-	}
+    public FancyIterator(Node<Item> startNode) {
+      nextNode = startNode;
+    }
 
+    @Override
+    public boolean hasNext() {
+      return nextNode != null;
+    }
+
+    @Override
+    public Item next() {
+      Node<Item> save = nextNode;
+      nextNode = nextNode.next;
+      return save.item;
+    }
+
+  }
 }
